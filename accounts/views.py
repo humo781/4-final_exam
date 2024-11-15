9365e9879b4a1cc7d73640e315d0ced5ec565a16
Khumoyun <humoyunortiqov05@gmail.com>
2024-11-15 09:28:21 +0500

    RegisterView, login_view created in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import RegisterForm
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'login.html')
+
+class RegisterView(CreateView):
+    context_object_name = 'form'
+    template_name = 'register.html'
+    form_class = RegisterForm
+
+    def form_invalid(self, form):
+        print("Form errors:", form.errors)
+        return super().form_invalid(form)
