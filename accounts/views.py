19896b1879d47b3a3464f2ab4ec73288c03350ad
Khumoyun <humoyunortiqov05@gmail.com>
2024-11-15 19:15:41 +0500

    CustomUser registered in admin.py, LOGIN_REDIRECT_URL added it direct the user when they authenticated, RegisterView have been given success_url, logic part of Login_view changed to better of itself in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,25 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import RegisterForm
 
-def login_view(request):
-    return render(request, 'login.html')
+class UserLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = "login.html"
+
+    def form_invalid(self, form):
+        print("Form errors:", form.errors)
+        return super().form_invalid(form)
+
+def main_page(request):
+    return render(request, 'main-page.html')
 
 class RegisterView(CreateView):
     context_object_name = 'form'
     template_name = 'register.html'
     form_class = RegisterForm
+    success_url = 'login'
 
     def form_invalid(self, form):
         print("Form errors:", form.errors)
